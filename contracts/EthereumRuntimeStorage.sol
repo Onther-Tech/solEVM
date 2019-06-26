0a92ea75e371e352a8f3dd30ba4e4d78517d2478
Jake Song <mubok0212@gmail.com>
2019-06-26 16:10:14 +0900

    contracts, test/contracts: add storage field

---
+++ contracts/EthereumRuntimeStorage.sol
@@ -0,0 +1,122 @@
+pragma solidity ^0.5.2;
+pragma experimental ABIEncoderV2;
+
+
+import { EVMCode } from "./EVMCode.slb";
+import { EVMStack } from "./EVMStack.slb";
+import { EVMMemory } from "./EVMMemory.slb";
+import { EVMStorage } from "./EVMStorage.slb";
+import { HydratedRuntime } from "./HydratedRuntime.sol";
+
+
+contract EthereumRuntimeStorage is HydratedRuntime {
+
+    struct EVMPreimage {
+        address code;
+        bytes data;
+        uint gasLimit;
+        uint pc;
+        uint8 errno;
+        uint gasRemaining;
+        uint stepCount;
+        bytes32[] stack;
+        bytes32[] mem;
+        bytes32[] tStorage;
+        bytes returnData;
+    }
+
+    struct EVMResult {
+        uint gas;
+        bytes data;
+        bytes lastRet;
+        bytes returnData;
+        uint8 errno;
+        bytes32[] mem;
+        bytes32[] stack;
+        uint pc;
+        bytes32[] tStorage;
+        bytes32 hashValue;
+    }
+
+    // Init EVM with given stack and memory and execute from the given opcode
+    // solhint-disable-next-line function-max-lines
+    function execute(EVMPreimage memory img) public returns (EVMResult memory) {
+        // solhint-disable-next-line avoid-low-level-calls
+        EVM memory evm;
+
+        initHydratedState(evm);
+
+        evm.context = Context(
+            DEFAULT_CALLER,
+            0,
+            // block gas limit
+            img.gasLimit,
+            0,
+            0,
+            0,
+            0
+        );
+
+        evm.data = img.data;
+        evm.gas = img.gasRemaining;
+
+        evm.caller = DEFAULT_CALLER;
+        evm.target = DEFAULT_CONTRACT_ADDRESS;
+
+        evm.code = EVMCode.fromAddress(img.code);
+        evm.stack = EVMStack.fromArray(img.stack);
+        evm.mem = EVMMemory.fromArray(img.mem);
+
+        _run(evm, img.pc, img.stepCount);
+
+        Context memory context = evm.context;
+        bytes32 hashValue = stateHash(evm, context);
+        EVMResult memory resultState;
+
+        resultState.gas = evm.gas;
+        resultState.data = evm.data;
+        resultState.lastRet = evm.lastRet;
+        resultState.returnData = evm.returnData;
+        resultState.errno = evm.errno;
+        resultState.mem = EVMMemory.toArray(evm.mem);
+        resultState.stack = EVMStack.toArray(evm.stack);
+        resultState.pc = evm.pc;
+        resultState.hashValue = hashValue;
+
+        return resultState;
+    }
+
+    function stateHash(EVM memory evm, Context memory context) internal view returns (bytes32) {
+        bytes32 contextHash = keccak256(abi.encodePacked(
+            context.origin,
+            context.gasPrice,
+            context.gasLimit,
+            context.coinBase,
+            context.blockNumber,
+            context.time
+        ));
+
+        bytes32 dataHash = keccak256(abi.encodePacked(
+            evm.gas,
+            evm.code.toBytes(),
+            evm.data,
+            evm.lastRet,
+            evm.returnData,
+            evm.errno
+        ));
+
+        HydratedState memory hydratedState = getHydratedState(evm);
+
+        bytes32 hashValue = keccak256(abi.encodePacked(
+            dataHash,
+            hydratedState.stackHash,
+            hydratedState.memHash,
+            evm.mem.size,
+            evm.stack.size,
+            evm.pc,
+            contextHash
+        ));
+
+        return hashValue;
+    }
+}
---
+++ test/contracts/ethereumRuntimeStorage.js
@@ -0,0 +1,48 @@
+const { getCode, deployContract, deployCode } =
+  require('./../helpers/utils');
+const fixtures = require('./../fixtures/runtime');
+const Runtime = require('./../../utils/EthereumRuntimeAdapter');
+const OP = require('./../../utils/constants');
+
+const { PUSH1, BLOCK_GAS_LIMIT } = OP;
+
+const EthereumRuntime = artifacts.require('EthereumRuntimeStorage.sol');
+
+contract('Runtime', function () {
+  let rt;
+
+  before(async () => {
+    rt = new Runtime(await deployContract(EthereumRuntime));
+  });
+
+  describe('executeAndStop', () => {
+    it('should allow to run a specific number of steps', async () => {
+      // codepointers: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, B, C
+      // execution order: 0, 1, 2, 8, 9, A, B, 3, 4, 5, 6, 7, C
+      const code = [
+        OP.PUSH1, '08', OP.JUMP, // jump to 0x08
+        OP.JUMPDEST, OP.GASLIMIT, OP.PUSH1, '0C', OP.JUMP, // 0x03. Jump to 0x0c
+        OP.JUMPDEST, OP.PUSH1, '03', OP.JUMP, // 0x08. Jump to 0x03
+        OP.JUMPDEST, // 0x0c
+      ];
+      const data = '0x';
+      const codeContract = await deployCode(code);
+      const executeStep = async (stepCount) =>
+        (await rt.execute(
+          {
+            code: codeContract.address,
+            data: data,
+            pc: 0,
+            stepCount: stepCount,
+          }
+        ));
+
+        const result = await executeStep(1);
+        console.log(result);
+      
+    });
+  });
+});
+  
+
+  
