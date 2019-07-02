5e8ee363d5f381ef996e970bfb0f158e4197489e
Jake Song <mubok0212@gmail.com>
2019-07-02 20:40:17 +0900

    contracts: convert storage struct to bytes32 and test

--- contracts/EVMStorageTest2.sol
+++ contracts/EVMStorageTest2.sol
@@ -2,26 +2,26 @@ pragma solidity ^0.5.0;
 
 import {EVMStorageToArray} from "./EVMStorageToArray.slb";
 
-contract EVMStorageTest2 {
+contract EVMStorageTest {
 
     using EVMStorageToArray for EVMStorageToArray.Storage;
     using EVMStorageToArray for EVMStorageToArray.Element;
-
-    function testToArray() public payable returns (bool ret) {
+    
+    function testToArrayForHash() public payable returns (bool ret) {
         ret = true;
         EVMStorageToArray.Storage memory sge;
         sge.store(0x01, 0x02);
         sge.store(0x03, 0x00);
         sge.store(0x05, 0x06);
-
+        
         bytes32[] memory sgeArr = sge.toArrayForHash();
-        assert(sgeArr.length == 3);
-        assert(sgeArr[0] == byte(0x01));
-        assert(sgeArr[1] == byte(0x02));
-        assert(sgeArr[2] == byte(0x03));
-        assert(sgeArr[3] == byte(0x00));
-        assert(sgeArr[4] == byte(0x05));
-        assert(sgeArr[5] == byte(0x06));
+        
+        assert(sgeArr.length == 6);
+        assert(sgeArr[0] == bytes32(uint(0x01)));
+        assert(sgeArr[1] == bytes32(uint(0x02)));
+        assert(sgeArr[2] == bytes32(uint(0x03)));
+        assert(sgeArr[3] == bytes32(uint(0x00)));
+        assert(sgeArr[4] == bytes32(uint(0x05)));
+        assert(sgeArr[5] == bytes32(uint(0x06)));
     }
-
 }
