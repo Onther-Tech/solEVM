f49c31907cef93332d0e8d0c56d4417e61fd54c9
Jake Song <mubok0212@gmail.com>
2019-10-01 01:59:07 +0900

    install truffle for debug

--- truffle-config.js
+++ truffle-config.js
@@ -7,7 +7,7 @@ module.exports = {
       settings: {
         optimizer: {
           enabled: true,
-          runs: 2,
+          runs: 200,
         },
         evmVersion: 'constantinople',
       },
@@ -15,19 +15,21 @@ module.exports = {
   },
   networks: {
     goerli: {
-      url: 'https://goerli.infura.io/v3/' + process.env.INFURA_API_KEY,
+      host: 'https://goerli.infura.io/v3/' + process.env.INFURA_API_KEY,
       network_id: '5', // eslint-disable-line camelcase
       gas: 6465030,
       gasPrice: 10000000000,
     },
     ganache: {
-      url: 'http://localhost:8111',
+      host: "127.0.0.1",
+      port: 8111,
       network_id: '*', // eslint-disable-line camelcase
       gas: 7000000,
       gasPrice: 1,
     },
     geth: {
-      url: 'http://localhost:8222',
+      host: "127.0.0.1",
+      port: 8222,
       network_id: '*', // eslint-disable-line camelcase
       gas: 7000000,
       gasPrice: 1,
