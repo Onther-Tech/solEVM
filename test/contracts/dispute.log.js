27356e36707c5dbe1bdecdb350eb214c99db8b64
Jake Song <mubok0212@gmail.com>
2020-01-14 14:28:13 +0900

    test/contracts: add test files per steps of implementation on Readme

--- test/contracts/dispute.log.js
+++ test/contracts/dispute.log.js
@@ -5,7 +5,7 @@ const debug = require('debug')('vgame-test');
 const assert = require('assert');
 
 const { Merkelizer, ExecutionPoker, Constants, FragmentTree } = require('./../../utils');
-const disputeFixtures = require('./../fixtures/disputeERC20');
+const disputeFixtures = require('./../fixtures/dispute.log');
 const { onchainWait, deployContract, deployCode, wallets, provider } = require('./../helpers/utils');
 
 const Verifier = require('./../../build/contracts/VerifierStorage.json');
