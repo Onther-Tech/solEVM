cfc6f238da0c252dba7ffde4799f6bdc48b7b463
Jake Song <mubok0212@gmail.com>
2020-01-31 17:09:57 +0900

    test/utils, utils: clean up

--- utils/testBytecodeLength.js
+++
@@ -1,4 +0,0 @@
-const Verifier = require('./../build/contracts/VerifierStorage.json');
-console.log('Verifier Bytecode Length', (Verifier.deployedBytecode.length / 2).toString(16));
-const Enforcer = require('./../build/contracts/EnforcerStorage.json');
-console.log('Enforcer Bytecode Length', Enforcer.deployedBytecode.length.toString(16));
